1d91e8d508246fe39751e3a0e839f612a0314aa4
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-17 03:51:02 +0530

    [Nikhil Kumar]:SingleCastDelegate Program Added

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -11,6 +11,36 @@ namespace Delegates
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Delegates Program");
+            Console.WriteLine("1.SingleCastDelegate" +
+                              "\n2.Exit");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter option");
+                int choose=Convert.ToInt32(Console.ReadLine());
+
+                switch (choose)
+                {
+                    case 1:
+                        SingleCast1 singleCast1 = new SingleCast1(SingleCastDelegate.Addition);
+                        int number=singleCast1(7, 8);
+                        Console.WriteLine(number);
+
+                        SingleCast2 singleCast2=new SingleCast2(SingleCastDelegate.Name);
+                        string name = singleCast2("Nikhil","Kumar");
+                        Console.WriteLine(name);
+                        break;
+
+                    case 2:
+                        flag= false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+
+                }
+            }
         }
     }
 }
---
+++ Delegates/SingleCastDelegate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate int SingleCast1(int firstnumber,int secondnumber);
+    public delegate string SingleCast2(string firstname, string secondname);
+    public class SingleCastDelegate
+    {
+        public static int Addition(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        public static string Name(string firstname, string secondname)
+        {
+            return firstname +" "+ secondname;
+        }
+    }
+}
